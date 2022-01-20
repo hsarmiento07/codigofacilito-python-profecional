feacef0e94b4be4cdb9ed48e76538296d1ab11ff
Howerd <correo>
2022-01-20 13:46:15 -0400

    main acido modificado

---
+++ seccion-2/main.py
@@ -0,0 +1,10 @@
+from re import T
+
+
+listas_Strings = ["Howerd", "Sarmiento", "Eduardo"]
+
+listas_enteros = [-19, 0, 10, 200]
+
+listas_booleanos = [True, True,  (2 < 10), (4>3 and 10 != 11) ]
+
+listas_floats = [10.0, 20.0, 30.0]
