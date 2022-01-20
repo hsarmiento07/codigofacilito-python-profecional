e7c8542a9974a0e8bea553df3211ff8d2b0e72a2
Howerd <correo>
2022-01-20 13:09:16 -0400

    la entrada acido actualizada

---
+++ entrada.py
@@ -0,0 +1,17 @@
+
+nombre_completo = str(input('Ingresa tu nombre completo: '))
+nombre_completo = (nombre_completo)
+
+edad = int(input('ingresa tu edad: '))
+
+altura =  float(input('ingresar tu altura: '))
+
+autorización = input('¿Autorisas el programa? (si/no)') == 'si'
+
+print(autorización)
+
+print(nombre_completo)
+
+print(edad)
+
+print(altura)
