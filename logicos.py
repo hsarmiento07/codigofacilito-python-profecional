aaa4ec7fb575231b863cfa633211364b4d01a174
Howerd <correo>
2022-01-20 11:31:05 -0400

    logicos acido agregado

---
+++ logicos.py
@@ -0,0 +1,10 @@
+# and, or y not
+
+resultado_final = True and True and 100 > 20
+print(resultado_final)
+
+resultado_final = True and (False or 50 > 10)
+print(resultado_final)
+
+resultado_final = not True
+print(resultado_final)
