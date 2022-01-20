8f6bdd72fa7e3b4fe06d21fd2ee5196ec62e7bc3
Howerd <correo>
2022-01-20 10:15:02 -0400

    cambios el archivo tipos

---
+++ tipos.py
@@ -0,0 +1,36 @@
+# String
+titulo_curso = 'Curso Profecional de Python'
+print(titulo_curso)
+
+nombre_completo = 'howerd Sarmiento'
+print(nombre_completo)
+
+mensaje = """Te encuentras
+en el curso: profesional de Python.
+En CodigoFacilito"""
+print(mensaje)
+
+# int
+# Al dividir con / obtenemos un tipo de dato flotante
+# Al dividir con / obtenemos un tipo de dato flotante
+numero_1 = -10
+print(numero_1)
+numero_2 = 10 * 20
+print(numero_2)
+numero_3 = 10 + 20
+print(numero_3)
+numero_4 = 10 - 20
+print(numero_4)
+numero_5 = 10 / 3
+print(numero_5)
+numero_6 = 10 // 3
+print(numero_6)
+
+# Float
+numero_dos =-3.14
+print(numero_dos)
+
+# Bool
+# true - false
+valor = True
+print(valor)
