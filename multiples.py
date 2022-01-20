1d0393b0580c53cbc4083305f2ac76adf2f2c793
Howerd <correo>
2022-01-20 13:22:46 -0400

    cambio de carpeta

--- multiples.py
+++
@@ -1,7 +0,0 @@
-nombre = 'Howerd'
-apellido = 'Sarmiento'
-titulo = 'Mr'
-
-print(nombre)
-print(apellido)
-print(titulo)
---
+++ seccion1/multiples.py
@@ -0,0 +1,5 @@
+nombre, apellido, titulo  = 'howerd','sarmiento','Mr'
+
+print(nombre)
+print(apellido)
+print(titulo)
