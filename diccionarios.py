bda9e45c7d596a7b0cd44b7d70ffbe4cc33d5d07
Howerd <correo>
2022-01-24 15:43:36 -0400

    diccionarios.py fue agregado

---
+++ diccionarios.py
@@ -0,0 +1,8 @@
+elementos = {'a': 1, 'b': 2, 'c':3, 'a': 4}
+"""
+elementos['nombre'] ='Cody'
+elementos[(1, 2, 3)] ='La llave es una tupla'
+
+elementos['nombre'] = 'codigoFacilito'
+"""
+print(elementos)
