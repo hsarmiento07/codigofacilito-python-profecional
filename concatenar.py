121b5e98ed8026cbb20a663cf17bb1bf5a1ee384
Howerd <correo>
2022-01-24 13:52:59 -0400

    concatenar.py fue agregado

--- concatenar.py
+++ concatenar.py
@@ -5,10 +5,12 @@ apellido = 'Sarmiento'
 
 # nombre_completo = 'Mr. %s %s %s.' %(nombre, apellido, 'Cordero')
 
-nombre_completo = 'Mr. {nombre} {apellido} {segundo_apellido}.'.format(
-    nombre=nombre,
-    apellido=apellido,
-    segundo_apellido='cordero'
-    )
+#nombre_completo = 'Mr. {nombre} {apellido} {segundo_apellido}.'.format(
+#    nombre=nombre,
+#    apellido=apellido,
+#    segundo_apellido='cordero'
+#    )
+
+nombre_completo = f'Mr. {nombre} {apellido} {10 * 20}'
 
 print(nombre_completo)
