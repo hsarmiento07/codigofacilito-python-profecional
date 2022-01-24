48e65584a8ae2b8b9e60fc9757cd6c9402de5277
Howerd <correo>
2022-01-24 16:36:56 -0400

    llaves agregadas

---
+++ llaves.py
@@ -0,0 +1,17 @@
+from xml.dom.minidom import Element
+
+
+diccionario = {'a': 1, 'b': 2, 'c':3, 'd': 4}
+
+# keys
+# values
+# items
+
+llaves = tuple(diccionario.keys())
+print(llaves)
+
+valores = tuple(diccionario.values())
+print(valores)
+
+elementos = tuple(diccionario.items())
+print(elementos)
