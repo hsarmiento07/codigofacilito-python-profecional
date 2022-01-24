10e85de1dc45ba250a08055812292c708968027d
Howerd <correo>
2022-01-24 09:50:30 -0400

    tuplas.py agrgadas

---
+++ tuplas.py
@@ -0,0 +1,14 @@
+#             0        1        2          3         4
+from re import U
+
+
+cursos = ('Python', 'Flask', 'Django', 'Raisls', 'MongoDB')
+
+primer_curso = cursos[0]
+print(primer_curso)
+
+ultimo_curso = cursos[-1]
+print(ultimo_curso)
+
+sub_tupla = cursos[0:3]
+print(sub_tupla)
