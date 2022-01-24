73aeba9593888bff4cc80a5e110640284d17028d
Howerd <correo>
2022-01-24 10:47:31 -0400

    descomprimir.py actualizado

---
+++ descomprimir.py
@@ -0,0 +1,12 @@
+# * lista
+# _ omitir objetos
+numeros = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+uno, dos, tres, cuatro, *resto_valores, nueve, diez = numeros
+
+print(uno)
+print(dos)
+print(tres)
+print(cuatro)
+print(resto_valores)
+print(nueve)
+print(diez)
