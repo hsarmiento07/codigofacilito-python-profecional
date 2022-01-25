3ade8f1f700a3f33a56fab2cf9caa6b35084533f
Howerd <correo>
2022-01-25 10:12:12 -0400

    condiciones.py acido agregada

---
+++ condiciones.py
@@ -0,0 +1,13 @@
+import re
+from xmlrpc.client import Boolean
+
+
+resultado = 11
+
+resultado = resultado > 10
+
+if resultado >  10 and resultado < 20:
+    print('La variable es muy alta')
+
+else:
+    print('la variable es muy baja')
