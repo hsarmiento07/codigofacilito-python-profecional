2f37da80ae152ac35944fe52e060804b7a2bf120
Howerd <correo>
2022-01-25 13:40:51 -0400

    range.py acido agregado

---
+++ range.py
@@ -0,0 +1,9 @@
+# star = 0, end, skips
+
+#for valor in range(5, 21, 3):
+#    print(valor)
+
+numeros = [10, 20, 30, 40, 50]
+
+for indice, numero in enumerate(numeros, 10):
+    print(indice, numero)
