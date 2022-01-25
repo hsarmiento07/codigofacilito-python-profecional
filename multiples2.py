d3ea0ed9e3c9b1ed59dadc5b9a9383ba52d6b7f8
Howerd <correo>
2022-01-25 10:34:16 -0400

    multiples2.py acido agregado

---
+++ multiples2.py
@@ -0,0 +1,10 @@
+calificacion =  int(input("ingrese un numero: "))
+
+if calificacion == 10:
+    print('Felicidades, aprobaste la materia con una calificacion perfecta')
+elif calificacion == 9 or calificacion == 8:
+    print('Felizidades, aprobaste la materia  ')
+elif calificacion == 7 or calificacion == 6:
+    print('aprobaste la materia')
+else:
+    print('Reprobaste la marteria')
