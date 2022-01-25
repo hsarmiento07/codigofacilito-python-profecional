4ef3448c33a11426fb41b55678f97dd3fa90a676
Howerd <correo>
2022-01-25 09:47:47 -0400

    none.py acido agregado

---
+++ none.py
@@ -0,0 +1,21 @@
+import re
+
+
+resultado = None # Falso
+
+resultado = [1, 2, 3]
+print(resultado)
+print(type(resultado))
+
+# 3 True (1) / False (0)
+
+"""
+False
+None
+0
+0.0
+'' / ""
+[]
+()
+{}
+"""
