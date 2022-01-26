153ba7d01b3b7606c565785b6f86f6f00f63ce37
Howerd <correo>
2022-01-26 13:55:19 -0400

    documentar.py acido agregado

---
+++ documentar.py
@@ -0,0 +1,29 @@
+# Docsting
+# __doc__ (Modulos, Clases, Métodos y Funciones)
+
+def suma(numero_1, numero_2):
+    """
+    la funcion suma 2 numeros enteros.
+
+    Argumentos:
+    numero_1 (int)
+    numero_2 (int)
+
+    se retorna la suma de los parametros.
+
+    >>> suma(10, 20)
+    30
+
+    >>> suma(100, 200)
+    300
+
+    """
+    return numero_1 + numero_2
+
+
+def resta(numero_1, numero_2):
+    """
+    >>> resta(100, 200)
+    -100
+    """
+    return None
